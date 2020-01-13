7e1f919d5a7ad8d2b11eb3073581fe09db02b8ba
neoql <nql1156325827@163.com>
2020-01-13 15:58:27 +0800

    1. 添加Seq2seqConfig代码 2. 添加Seq2seq代码

---
+++ open_couplet/config.py
@@ -0,0 +1,25 @@
+import json
+
+
+class Seq2seqConfig(object):
+    def __init__(self, **cfg):
+        self.vocab_size = cfg.get('vocab_size', 10000)
+        self.hidden_size = cfg.get('hidden_size', 700)
+        self.rnn_layers = cfg.get('rnn_layers', 2)
+        self.cnn_kernel_size = cfg.get('cnn_kernel_size', 3)
+        self.dropout_p = cfg.get('dropout_p', 0.7)
+        self.rnn_cell = cfg.get('rnn_cell', 'lstm')
+
+    @classmethod
+    def from_config(cls, filename):
+        config = cls()
+        with open(filename, 'r') as fp:
+            config.__dict__ = json.load(fp)
+        return config
+
+    def save_config(self, filename):
+        with open(filename, 'w') as fp:
+            json.dump(self.__dict__, fp, indent=2)
+
+    def __repr__(self):
+        return f'Seq2seqConfig({self.__dict__})'
--- open_couplet/models/seq2seq.py
+++ open_couplet/models/seq2seq.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 
 from typing import Optional, Tuple
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
-from .attention import ScaledDotProductAttention
+from open_couplet.models.attention import ScaledDotProductAttention
+from open_couplet.config import Seq2seqConfig
 
 
 def _rnn(rnn_cell, *args, **kwargs):
@@ -74,34 +75,34 @@ class CNN(nn.Module):
 
 
 class Encoder(nn.Module):
-    __constants__ = ['units', 'rnn_layers', 'dropout_p', 'rnn_cell']
+    __constants__ = ['hidden_size', 'rnn_layers', 'dropout_p', 'rnn_cell']
 
-    def __init__(self, embedding, units, rnn_layers=1, cnn_kernel_size=3, dropout_p=0.0, rnn_cell='gru'):
+    def __init__(self, embedding, hidden_size, rnn_layers=1, cnn_kernel_size=3, dropout_p=0.0, rnn_cell='gru'):
         super(Encoder, self).__init__()
         rnn_cell = rnn_cell.lower()
 
         self.embedding: nn.Embedding = embedding
-        self.units = units
+        self.hidden_size = hidden_size
         self.rnn_layers = rnn_layers
         self.dropout_p = dropout_p
         self.rnn_cell = rnn_cell
 
-        self.cnn = CNN(units, units, units, kernel_size=cnn_kernel_size, dropout_p=dropout_p)
+        self.cnn = CNN(hidden_size, hidden_size, hidden_size, kernel_size=cnn_kernel_size, dropout_p=dropout_p)
         self.rnn = _rnn(
             rnn_cell=rnn_cell,
-            input_size=units,
-            hidden_size=units,
+            input_size=hidden_size,
+            hidden_size=hidden_size,
             num_layers=rnn_layers,
             dropout=dropout_p,
             bidirectional=True,
         )
 
-        self.norm_1 = nn.LayerNorm(units)
-        self.norm_2 = nn.LayerNorm(units)
+        self.norm_1 = nn.LayerNorm(hidden_size)
+        self.norm_2 = nn.LayerNorm(hidden_size)
 
-        self.h_proj = nn.Linear(2 * units, units, bias=False)
+        self.h_proj = nn.Linear(2 * hidden_size, hidden_size, bias=False)
         if rnn_cell == 'lstm':
-            self.c_proj = nn.Linear(2 * units, units, bias=False)
+            self.c_proj = nn.Linear(2 * hidden_size, hidden_size, bias=False)
 
         self.reset_parameters()
 
@@ -115,9 +116,9 @@ class Encoder(nn.Module):
         :param enforce_sorted: if True, the source is expected to contain sequences sorted by
             length in a decreasing order. If False, this condition is not checked. Default: True.
         :return: context, hidden
-            **context** (batch_size, src_len, units * 2):
+            **context** (batch_size, src_len, hidden_size):
                 tensor containing the encoded features of the source sequence
-            **state** (layers, batch, units * 2):
+            **state** (layers, batch, hidden_size):
                 tensor containing the features in the hidden state `h`
         """
         batch_size, src_len = source.size()
@@ -136,7 +137,7 @@ class Encoder(nn.Module):
         # RNN layer
         rnn_input = pack_padded_sequence(cnn_out, seq_len, batch_first=True, enforce_sorted=enforce_sorted)
         context, state = self.rnn(rnn_input)
-        # (batch_size, src_len, 2, units)
+        # (batch_size, src_len, 2, hidden_size)
         context, _ = pad_packed_sequence(context, batch_first=True).view(batch_size, src_len, 2, -1)
         if self.dropout_p:
             context = F.dropout(context, p=self.dropout_p, training=self.training)
@@ -159,62 +160,66 @@ class Encoder(nn.Module):
 
 # noinspection DuplicatedCode
 class Decoder(nn.Module):
-    __constants__ = ['units', 'rnn_layers', 'cnn_kernel_size', 'dropout_p']
+    __constants__ = ['hidden_size', 'rnn_layers', 'cnn_kernel_size', 'dropout_p']
 
-    def __init__(self, embedding: nn.Embedding, units: int, cnn_kernel_size: int = 3,
+    def __init__(self, embedding: nn.Embedding, hidden_size: int, cnn_kernel_size: int = 3,
                  rnn_layers: int = 1, dropout_p: float = 0, rnn_cell: str = 'gru'):
         super(Decoder, self).__init__()
 
         self.embedding = embedding
-        self.units = units
+        self.hidden_size = hidden_size
         self.rnn_layers = rnn_layers
         self.cnn_kernel_size = cnn_kernel_size
         self.dropout_p = dropout_p
         self.rnn_cell = rnn_cell.lower()
 
-        self.cnn = CNN(units, units, units, kernel_size=cnn_kernel_size,
+        self.cnn = CNN(hidden_size, hidden_size, hidden_size, kernel_size=cnn_kernel_size,
                        dropout_p=dropout_p, mid=False)
         self.rnn = _rnn(
             rnn_cell=rnn_cell,
-            input_size=units * 2,
-            hidden_size=units,
+            input_size=hidden_size * 2,
+            hidden_size=hidden_size,
             num_layers=rnn_layers,
             dropout=dropout_p,
         )
 
-        self.norm_1 = nn.LayerNorm(units)
-        self.norm_2 = nn.LayerNorm(units)
-        self.norm_3 = nn.LayerNorm(units)
+        self.norm_1 = nn.LayerNorm(hidden_size)
+        self.norm_2 = nn.LayerNorm(hidden_size)
+        self.norm_3 = nn.LayerNorm(hidden_size)
 
-        self.q_proj = nn.Linear(units, units, bias=False)
+        self.q_proj = nn.Linear(hidden_size, hidden_size, bias=False)
         self.attention = ScaledDotProductAttention()
 
-        self.fc_1 = nn.Linear(units, units)
-        self.fc_2 = nn.Linear(units, embedding.num_embeddings)
+        self.fc_1 = nn.Linear(hidden_size, hidden_size)
+        self.fc_2 = nn.Linear(hidden_size, embedding.num_embeddings)
 
         self.reset_parameters()
 
     def reset_parameters(self):
         pass
 
-    def forward(self, input_seq: torch.Tensor,
-                context: torch.Tensor, state: torch.Tensor, fh: Optional[torch.Tensor] = None,
-                pad_mask: Optional[torch.Tensor] = None, cnn_mem: Optional[Tuple[torch.Tensor, torch.Tensor]] = None):
+    def forward(self,
+                input_seq: torch.Tensor,
+                context: torch.Tensor,
+                state: torch.Tensor,
+                fh: Optional[torch.Tensor] = None,
+                attention_mask: Optional[torch.Tensor] = None,
+                cnn_mem: Optional[Tuple[torch.Tensor, torch.Tensor]] = None):
         """
         :param input_seq: (batch, tgt_len)
-        :param context: (layers, batch, units)
-        :param state: (batch_size, src_len, units)
-        :param fh: (batch_size, units)
-        :param pad_mask: (batch_size, src_fix_len)
-        :param cnn_mem: (batch_size, units, mem_len)
+        :param context: (layers, batch, hidden_size)
+        :param state: (batch_size, src_len, hidden_size)
+        :param fh: (batch_size, hidden_size)
+        :param attention_mask: (batch_size, src_fix_len)
+        :param cnn_mem: (batch_size, hidden_size, mem_len)
         :return:
         """
         batch_size, tgt_len = input_seq.size()
         if fh is None:
-            fh = torch.zeros(batch_size, self.units).to(input_seq.device)
+            fh = torch.zeros(batch_size, self.hidden_size).to(input_seq.device)
 
         # Embedding layer
-        # embedded: (batch_size, tgt_len, units)
+        # embedded: (batch_size, tgt_len, hidden_size)
         embedded = self.embedding(input_seq)
         if self.dropout_p:
             embedded = F.dropout(embedded, p=self.dropout_p, training=self.training)
@@ -229,7 +234,7 @@ class Decoder(nn.Module):
         attn_weights = []
 
         for i in range(tgt_len):
-            # (batch_size, tgt_len, 2*units)
+            # (batch_size, tgt_len, 2*hidden_size)
             input_step = torch.cat([cnn_out[:, i, :], fh], dim=-1).unsqueeze(1)
 
             rnn_out, state = self.rnn(input_step, state)
@@ -237,7 +242,7 @@ class Decoder(nn.Module):
                 rnn_out = F.dropout(rnn_out, p=self.dropout_p, training=self.training)
             rnn_out = self.norm_2(cnn_out[:, i, :].unsqueeze(1) + rnn_out)
 
-            attn_out, attn_w = self.attention(q=self.q_proj(rnn_out), k=context, v=context, mask=pad_mask)
+            attn_out, attn_w = self.attention(q=self.q_proj(rnn_out), k=context, v=context, mask=attention_mask)
             attn_out = self.norm_3(attn_out + rnn_out)
 
             out = gelu(self.fc_1(attn_out))
@@ -254,13 +259,26 @@ class Decoder(nn.Module):
 
 
 class Seq2seq(nn.Module):
-    def __init__(self, vocab_size, units, rnn_layers=1, cnn_kernel_size=3, dropout_p=0.0, rnn_cell='gru'):
+    def __init__(self, config: Seq2seqConfig):
         super(Seq2seq, self).__init__()
 
-        embedding = nn.Embedding(vocab_size, units)
+        embedding = nn.Embedding(config.vocab_size, config.hidden_size)
 
-        self.encoder = Encoder(embedding, units, rnn_layers, cnn_kernel_size, dropout_p, rnn_cell)
-        self.decoder = jit.script(Decoder(embedding, units, cnn_kernel_size, rnn_layers, dropout_p, rnn_cell))
+        self.encode = Encoder(embedding, config.hidden_size, config.rnn_layers, config.cnn_kernel_size,
+                               config.dropout_p, config.rnn_cell)
+        self.decode = jit.script(Decoder(embedding, config.hidden_size, config.cnn_kernel_size,
+                                          config.rnn_layers, config.dropout_p, config.rnn_cell))
 
-    def forward(self):
-        pass
+    def forward(self, x1, x2, x1_len, enforce_sorted=False):
+        attention_mask = self.attention_mask(x1_len, x1.size(1)) \
+            if x1_len.max().item() != x1_len.min().item() else None
+
+        context, state = self.encode(x1, x1_len, enforce_sorted)
+        prob, _, attn_weights = self.decode(x2, context, state, attention_mask=attention_mask)
+
+        return prob, attn_weights
+
+    @staticmethod
+    def attention_mask(length, fix_len):
+        mask = (torch.arange(0, fix_len) < length.unsqueeze(-1))
+        return mask
