7ac262d987c159251c1ec9df07d3e16ecab8b7cb
neoql <nql1156325827@163.com>
2020-02-13 14:43:01 +0800

    更新email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="AI for Chinese couplet",
     license="MIT Licence",
     author="QL Neo",
-    author_email="neoql@163.com",
+    author_email="neoql2333@163.com",
 
     packages=["open_couplet", "open_couplet.models"],
     install_requires=["torch==1.3.1"]
